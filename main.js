c6d3fd79d43860139d06616fd4ef6188d5eadccf
Anna <annaurbaniak18@gmail.com>
2021-11-21 11:53:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('.btn2')
+const square = document.querySelector('.color')
+const removeColor = document.querySelector('.remove-color')
+
+function redColor() {
+	/*square.classList.add('red')
+	square.classList.remove('blue')*/
+    square.classList.toggle('red')
+}
+
+function blueColor() {
+	square.classList.add('blue')
+	square.classList.remove('red')
+}
+function deleteColor() {
+	square.classList.remove('blue')
+    square.classList.remove('red')
+}
+
+btn1.addEventListener('click', redColor)
+btn2.addEventListener('click', blueColor)
+
+removeColor.addEventListener('click', deleteColor)
